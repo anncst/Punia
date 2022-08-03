5c0a5935b15ce2360b9a809105888d485919e0cd
Milena Szelcel <milenaszelcel@gmail.com>
2022-08-03 11:38:20 +0200

    fixed Button Component

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import styles from './Button.module.scss'
 
-const Button = ({ children }) => {
-  return <button className={styles.buttonBlue}>{children}</button>
+const Button = ({ children, color = 'blue' }) => {
+  return (
+    <button
+      className={classNames({
+        [styles.blue]: color === 'blue',
+        [styles.white]: color === 'white',
+      })}
+    >
+      {children}
+    </button>
+  )
 }
-Button.propTypes = { children: PropTypes.node }
+
+Button.propTypes = { children: PropTypes.node, color: PropTypes.string }
+
 export default Button
