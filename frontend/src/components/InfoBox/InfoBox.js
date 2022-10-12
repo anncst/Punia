24a464ea9d055a4323ee8336c4f97f83aadb5b2e
anncst <milenaszelcel@gmail.com>
2022-10-12 10:29:43 +0200

    swap paddingo into gap, added var color to a arrow

--- frontend/src/components/InfoBox/InfoBox.js
+++ frontend/src/components/InfoBox/InfoBox.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import styles from './InfoBox.module.scss'
 
-const InfoBox = ({ text, value, date, unit, change, changeUnit }) => {
-  return (
-    <div className={styles.box}>
-      <div className={styles.textBox}>
-        <div className={styles.text}>{text}</div>
-        <div className={styles.date}>{date}</div>
+const InfoBox = ({ text, value, date, unit, change, changeUnit, color }) => (
+  <div className={styles.box}>
+    <div className={styles.textBox}>
+      <div className={styles.text}>{text}</div>
+      <div className={styles.date}>{date.toLocaleString()}</div>
+    </div>
+    <div className={styles.contentBox}>
+      <div className={styles.value}>
+        {value}
+        <span className={styles.unit}>{unit}</span>
       </div>
-      <div className={styles.contentBox}>
-        <div className={styles.value}>
-          {value}
-          <span className={styles.unit}>{unit}</span>
-        </div>
-        <div className={styles.change}>
-          <span className={styles.arrow}>▲</span>
-          {change}
-          {changeUnit}
-        </div>
+      <div className={styles.change}>
+        <span
+          className={classNames({
+            [styles.green]: color === 'green',
+            [styles.red]: color === 'red',
+          })}
+        >
+          {color === 'green' ? '▲' : '▼'}
+        </span>
+        {change}
+        {changeUnit}
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 InfoBox.propTypes = {
   text: PropTypes.string,
@@ -31,5 +37,6 @@ InfoBox.propTypes = {
   unit: PropTypes.string,
   change: PropTypes.number,
   changeUnit: PropTypes.string,
+  color: PropTypes.string,
 }
 export default InfoBox
