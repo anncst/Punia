27d2c2219d50e5d6610dc01c89f5b6edc8ac1f78
anncst <milenaszelcel@gmail.com>
2022-11-04 19:58:35 +0100

    fixed infobox number problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import { DateTime } from 'luxon'
 import { Home } from './components/Home'
 import Button from './components/Button/Button'
-import NumberInput from './components/NumberInput/NumberInput'
 import InfoBox from './components/InfoBox/InfoBox'
 
 function App() {
@@ -9,15 +9,23 @@ function App() {
     <div>
       <Home />
       <Button>Dodaj</Button>
-      <NumberInput placeholder="4,00" label="kg" max="10" step="0.01" />
-      <NumberInput placeholder="200" label="gram" max="10" />
       <InfoBox
         text="Waga"
-        value="3.50"
+        value={3.5}
         unit="kg"
-        change="200"
+        change={200}
         changeUnit="g"
-        date="wczoraj"
+        date={DateTime.now()}
+        color="red"
+      />
+      <InfoBox
+        text="Waga"
+        value={10}
+        unit="kg"
+        change={200}
+        changeUnit="g"
+        date={DateTime.now()}
+        color="green"
       />
     </div>
   )
