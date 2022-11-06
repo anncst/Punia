5154d182a5ceb9ea97bb14b247a124a1b64a61ec
milena <85757899+anncst@users.noreply.github.com>
2022-11-06 14:08:50 +0000

    added dropdown menu to the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Button from './components/Button/Button'
 import { Navbar } from './components/Navbar/Navbar'
+// eslint-disable-next-line no-unused-vars
+import { styles } from './App.module.scss'
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar text="Kocica wredna" />
       <Button>Dodaj</Button>
     </div>
   )
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import Hamburger from '../Hamburger/Hamburger'
 import styles from './Navbar.module.scss'
 
-export const Navbar = ({ text }) => (
-  <div className={styles.navbarContainer}>
-    <h1>{text}</h1>
-    <Hamburger />
-  </div>
-)
+export const Navbar = ({ text }) => {
+  const [isOpen, setOpen] = useState(false)
+  return (
+    <div
+      className={classNames(styles.navbarContainer, {
+        [styles.navbarOpen]: isOpen,
+      })}
+    >
+      <div className={styles.menu}>
+        <div className={styles.text}>{text}</div>
+        <Hamburger isOpen={isOpen} setOpen={setOpen} />
+      </div>
+      {isOpen ? (
+        <div className={styles.dropdown}>
+          <button className={styles.button}>Dodaj wagę</button>
+          <button className={styles.button}>Dodaj karmę</button>
+        </div>
+      ) : null}
+    </div>
+  )
+}
 
 Navbar.propTypes = {
   text: PropTypes.string,
