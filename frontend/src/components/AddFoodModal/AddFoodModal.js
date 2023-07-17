0f2bbf4b9b2857feb18d50a9a4b2ec0464189248
anncst <milenaszelcel@gmail.com>
2023-07-17 16:21:44 +0200

    added Radio in FoodModal

--- frontend/src/components/AddFoodModal/AddFoodModal.js
+++ frontend/src/components/AddFoodModal/AddFoodModal.js
@@ -3,14 +3,20 @@ import Button from '../Button/Button'
 import { Slider } from '../Slider/Slider'
 import { NumberInput } from '../NumberInput/NumberInput'
 import styles from './AddFoodModal.module.scss'
+import Radio from '../Radio/Radio'
 
 export const AddFoodModal = () => {
   const [value, setValue] = useState(50)
+  const [chosen, setChosen] = useState()
   return (
     <div className={styles.form}>
       <NumberInput label="gram" placeholder="120" />
       <div className={styles.orText}>lub</div>
-      {/* Tutaj Radio dać */}
+      <div className={styles.radioBox}>
+        <Radio id="200g" name="food" onChange={setChosen} />
+        <Radio id="400g" name="food" onChange={setChosen} />
+        <Radio id="600g" name="food" onChange={setChosen} />
+      </div>
       <div className={styles.slider}>
         <Slider min={1} max={100} value={value} setValue={setValue} />
       </div>
